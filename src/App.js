72ad50fc833262b5c6492d77f5d3c89e2f317c51
Sasha Murray <sasha.murray@appliedtechnology.se>
2019-10-30 20:29:17 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component} from 'react';
 import './App.css';
+import sha1 from 'js-sha1'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const hash = sha1('hunter2').toUpperCase();
+const hashPrefix = hash.substring(0, 5);
+const hashSuffix = hash.substring(5);
+
+const search = (body, hash) => {
+  const result = {pwned: false, count: 0};
+
+  // Every password hash is followed by a colon (:) and the password count
+  const pattern = new RegExp(hash + ':(\\d+)');
+  console.log(pattern)
+  const match = body.match(pattern);
+  console.log(match)
+  if (match) {
+    result.pwned = true;
+    result.count = match[1];
+  }
+  console.log(result)
+  return result;
+};
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      data: null,
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://api.pwnedpasswords.com/range/' + hashPrefix)
+    .then(response => response.text())
+    .then(body => search(body, hashSuffix))
+    .catch(error => console.log('ERR', error)) 
+  }
+
+  render() {
+
+    return (
+      <div className="App"> TEST RESULT IN CONSOLE</div>
   );
+}
 }
 
 export default App;
