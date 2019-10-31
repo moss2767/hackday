a6467cd9883446770f7b0bc31639a95c3424b6de
Sasha Murray <sasha.murray@appliedtechnology.se>
2019-10-31 14:35:16 +0100

    styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import { makeStyles } from '@material-ui/core/styles'
 import Header from './components/Header/Header'
 import Input from './components/Input/Input'
 import Card from './components/Card/Card'
+import { Container } from '@material-ui/core'
+import checkPassword from './logic'
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -11,18 +13,42 @@ const useStyles = makeStyles(theme => ({
   },
   button: {
     margin: theme.spacing(1)
+  },
+  card: {
+    minWidth: 275
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)'
+  },
+  title: {
+    fontSize: 14
+  },
+  pos: {
+    marginBottom: 12
   }
 }))
 
 const App = () => {
   const classes = useStyles()
 
+  const [pwdToCheck, setPwdToCheck] = useState('')
+
+  const [returnValue, setReturnValue] = useState('')
+
+  const helpers = {
+    handleChange: (event) => setPwdToCheck(event.target.value),
+    search: async () => setReturnValue(await checkPassword(pwdToCheck))
+  }
   return (
-    <div className={classes.root}>
+    <Container className={classes.root}>
       <Header />
-      <Input />
+      <Input helpers={helpers} pwdToCheck={pwdToCheck}/>
+      <h1> { returnValue.count ? (`Your password occurs in the database ${returnValue.count} times.`) : ''}</h1>
       <Card title='Test card' text='This is a boilerplate text about cryptography' />
-    </div>
+      <Card title='k-anonymity' text='"Given person-specific field-structured data, produce a release of the data with scientific guarantees that the individuals who are the subjects of the data cannot be re-identified while the data remain practically useful."' />
+    </Container>
   )
 }
 
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,25 +1,41 @@
 /* eslint-disable react/prop-types */
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
-import { Paper, Typography } from '@material-ui/core'
+import { Card, CardContent, Typography, CardActions, Button } from '@material-ui/core'
 
 const useStyles = makeStyles(theme => ({
   root: {
-    padding: theme.spacing(3, 2)
+    padding: theme.spacing(3, 2),
+    margin: theme.spacing(2, 2)
+  },
+  card: {
+    minWidth: 275
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)'
+  },
+  pos: {
+    marginBottom: 12
   }
-
 }))
-export default function Card (props) {
+export default function SimpleCard (props) {
   const classes = useStyles()
 
   return (
-    <Paper className={classes.root}>
-      <Typography variant="h5" component="h3">
-        {props.title}
-      </Typography>
-      <Typography component="p">
-        {props.text}
-      </Typography>
-    </Paper>
+    <Card className={classes.root}>
+      <CardContent>
+        <Typography className={classes.title} variant="h5">
+          {props.title}
+        </Typography>
+        <Typography component="p">
+          {props.text}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
   )
 }
